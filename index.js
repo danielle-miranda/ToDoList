cd5c9c85d58e5e5fa1382d4c41c4017b022a8aaa
Danielle Miranda <danielle.m.miranda96@gmail.com>
2024-11-12 15:27:49 -0800

    List created

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // index.js
-console.log("js is working today")
 
-$.ajax({
-  type: 'GET',
-  url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
-  dataType: 'json',
-  success: function (response, textStatus) {
-    console.log(response);
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
+$(document).ready(function(){
+  console.log('dom ready');
+
+  $.ajax({
+    type: 'GET',
+    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function(task) {
+        $('#todo-list').append('<p>' + task.content + '</p>');
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 });
