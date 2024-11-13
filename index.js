5de6a18863ddbbcde8c1afa31325225c1c5b705a
Danielle Miranda <danielle.m.miranda96@gmail.com>
2024-11-12 16:11:41 -0800

    Creating Tasks

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-// index.js
-
 $(document).ready(function(){
-  console.log('dom ready');
-
-  $.ajax({
-    type: 'GET',
-    url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=1',
-    dataType: 'json',
-    success: function (response, textStatus) {
-      response.tasks.forEach(function(task) {
-        $('#todo-list').append('<p>' + task.content + '</p>');
-      })
-    },
-    error: function (request, textStatus, errorMessage) {
-      console.log(errorMessage);
-    }
+  var getAndDisplayAllTasks = function () {
+    $.ajax({
+      type: 'GET',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
+  
+  var createTask = function () {
+    $.ajax({
+      type: 'POST',
+      url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=2',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#new-task-content').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        $('#new-task-content').val('');
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });  
+  }
+  
+  $('#create-task').on('submit', function (e) {
+    e.preventDefault();
+    createTask();
   });
+  
+  getAndDisplayAllTasks();
+  
 });
